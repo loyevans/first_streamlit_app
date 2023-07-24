d577d085fd12872aafb519a682092fb1e42b4b78
loyevans <loyevans@users.noreply.github.com>
2023-07-24 15:23:36 -0500

    Update streamlit_app.py
    mod: fruit load list

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,15 @@ streamlit.dataframe(fruityvice_normalized)
 # snowflake connection
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("select current_user(), current_account(), current_region()")
+# 1st exercise 
+#-- my_cur.execute("select current_user(), current_account(), current_region()")
+#-- my_cur.execute("select current_user(), current_account(), current_region()")
+#-- my_data_row = my_cur.fetchone()
+#-- streamlit.text("Hellow from Snowflake:")
+#-- streamlit.text(my_data_row)
+# 2nd exercise
+my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hellow from Snowflake:")
+streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
+
