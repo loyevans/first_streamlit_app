6d0a2832d35bbfdf2ef81c501c63a4394a79bcd5
loyevans <loyevans@users.noreply.github.com>
2023-07-24 15:25:20 -0500

    Update streamlit_app.py
    formatting

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,8 +43,13 @@ my_cur = my_cnx.cursor()
 #-- streamlit.text("Hellow from Snowflake:")
 #-- streamlit.text(my_data_row)
 # 2nd exercise
+#-- my_cur.execute("select * from fruit_load_list")
+#-- my_data_row = my_cur.fetchone()
+#-- streamlit.text("The fruit load list contains:")
+#-- streamlit.text(my_data_row)
+# 3rd exercise
 my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_row)
 
