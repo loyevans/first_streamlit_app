8998111b9bb5913c8f8753dc15e690ab6dd6f299
loyevans <loyevans@users.noreply.github.com>
2023-07-24 15:19:44 -0500

    Update streamlit_app.py
    added snowflake connector and used snowflake secret

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,3 +32,11 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output to the screen as a table
 streamlit.dataframe(fruityvice_normalized)
+
+# snowflake connection
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select current_user(), current_account(), current_region()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hellow from Snowflake:")
+streamlit.text(my_data_row)
