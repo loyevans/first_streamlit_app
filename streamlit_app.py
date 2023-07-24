6aa7517e7cea4beaff1fc071f8024a251361be52
loyevans <loyevans@users.noreply.github.com>
2023-07-24 11:08:09 -0500

    Update streamlit_app.py
    added fruit input text box to pass to fv get request

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 #new section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # removed response code - streamlit.text(fruityvice_response)
 # removed basic json body response - streamlit.text(fruityvice_response.json())
 #take response and normalize it
