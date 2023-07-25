813cdbfc46aef3778be5faabcb5fdfb0b4c115f3
loyevans <loyevans@users.noreply.github.com>
2023-07-25 10:21:25 -0500

    Update streamlit_app.py
    stop snowflake connector code while tshoot

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,25 +33,10 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output to the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
 # snowflake connection
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-# 1st exercise 
-#-- my_cur.execute("select current_user(), current_account(), current_region()")
-#-- my_cur.execute("select current_user(), current_account(), current_region()")
-#-- my_data_row = my_cur.fetchone()
-#-- streamlit.text("Hellow from Snowflake:")
-#-- streamlit.text(my_data_row)
-# 2nd exercise
-#-- my_cur.execute("select * from fruit_load_list")
-#-- my_data_row = my_cur.fetchone()
-#-- streamlit.text("The fruit load list contains:")
-#-- streamlit.text(my_data_row)
-# 3rd exercise
-#-- my_cur.execute("select * from fruit_load_list")
-#-- my_data_row = my_cur.fetchone()
-#-- streamlit.header("The fruit load list contains:")
-#-- streamlit.dataframe(my_data_row)
 # 4th exercise
 my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
@@ -61,3 +46,21 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding', add_my_fruit)
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
+
+# 1st exercise with snowflake connector 
+#-- my_cur.execute("select current_user(), current_account(), current_region()")
+#-- my_cur.execute("select current_user(), current_account(), current_region()")
+#-- my_data_row = my_cur.fetchone()
+#-- streamlit.text("Hellow from Snowflake:")
+#-- streamlit.text(my_data_row)
+# 2nd exercise with snowflake connector
+#-- my_cur.execute("select * from fruit_load_list")
+#-- my_data_row = my_cur.fetchone()
+#-- streamlit.text("The fruit load list contains:")
+#-- streamlit.text(my_data_row)
+# 3rd exercise with snowflake connector
+#-- my_cur.execute("select * from fruit_load_list")
+#-- my_data_row = my_cur.fetchone()
+#-- streamlit.header("The fruit load list contains:")
+#-- streamlit.dataframe(my_data_row)
