4c823fdc93423f76d0c92e6bc14b5d4b40f90784
loyevans <loyevans@users.noreply.github.com>
2023-07-25 14:22:21 -0500

    Update streamlit_app.py
    clean up title, button, insert and connection close statements

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ except URLError as e:
     streamlit.error()
 
 # snowflake connection
-streamlit.header("The fruit load list contains:")
+streamlit.header("View our Fruit List - and Add Your Favorites!")
 # snowflake-related function
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
@@ -51,9 +51,10 @@ def get_fruit_load_list():
         return my_cur.fetchall()
 
 # add button to load the fruit
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    my_cnx.close()
     streamlit.dataframe(my_data_rows)
 
 # allow end user to add fruit to the list
@@ -61,11 +62,12 @@ def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
         my_cur.execute("insert into fruit_load_list values ('"+ new_fruit+"')")
         return "Thanks for adding " + new_fruit
-
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
+    my_cnx.close()
     streamlit.text(back_from_function)
+
     
 
