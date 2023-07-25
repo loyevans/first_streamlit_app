8e6e699f7b56b5299e7809fd9a12c678e146a0cb
loyevans <loyevans@users.noreply.github.com>
2023-07-25 10:30:07 -0500

    Update streamlit_app.py
    added try/except for fruityvice input

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,18 +22,23 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # fruit list data frame
 streamlit.dataframe(fruits_to_show)
+
 #new section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# removed response code - streamlit.text(fruityvice_response)
-# removed basic json body response - streamlit.text(fruityvice_response.json())
-#take response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#output to the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+try: 
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    #take response and normalize it
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    #output to the screen as a table
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
+#pause while tshoot
 streamlit.stop()
 # snowflake connection
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
